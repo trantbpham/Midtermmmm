e6d5610a91245d46f1c2e16f1c330a34ab29e8c2
Tran <pham.tr@northeastern.edu>
2021-07-06 22:26:24 -0400

    add function to class & main function

--- Q1_BinarySearch.java
+++ Q1_BinarySearch.java
@@ -1,34 +1,43 @@
-import java.util.Scanner;
+public class Q1_BinarySearch() {
 
-public class BinarySearch() {
+    private int floor;
 
-        Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
-        int inputArray[] = new int[num];
-        for (int i = 0; i < num; i++) {
-            inputArray[i] = input.nextInt();
-        }
+    public Q1_BinarySearch(int x) {
+        floor = x;
+    }
+//        Scanner input = new Scanner(System.in);
+//        int num = input.nextInt();
+//        int inputArray[] = new int[num];
+//        for (int i = 0; i < num; i++) {
+//            inputArray[i] = input.nextInt();
+//        }
 
-
+    public int findFloor() {
         int first = 0;
-        int last = inputArray.length - 1;
+        int last = floor;
         int mid = 0;
 
         while (first <= last) {
             mid = (first + last ) / 2;
-            if (inputArray[mid] < num) {
+            if (mid < floor) {
                 first = mid + 1;
             }
-            if (inputArray[mid] > num) {
+            if (mid > floor) {
                 last = mid - 1;
             }
-            else {
-                return mid;
+            if (first > last) {
+                throw new IllegalStateException(floor + "Not Found");
             }
 
         }
+        return mid;
     }
 
+    public static void main(String[] args) {
+        int x = 1000;
+        Q1_BinarySearch array = new Q1_BinarySearch(x);
+        System.out.println("result" + array);
 
+    }
 
 }
